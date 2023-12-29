f11918cbccdfeb6a531c56f45d6eac5f4842cdaf
MANOJ KUMAR SANDRAMALLA <118067041+MANOJ2514@users.noreply.github.com>
2023-12-29 10:22:30 +0530

    main.py

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 import streamlit as st
 st.title("Hello World!")
+st.write("<h1>Hello world</h1>",unsafe_allow_html=True)
+#ing colour of test
+st.write("<h1ss style='color:red;'>hello world</h1>",unsafe_allow_html=True)
+st.file_uploader("upload file")
